ed220194ec752abfaf70b2f4dbbd9b3700f94f00
misaki. <misakiigmd@gmail.com>
2024-03-22 09:31:06 +0100

    Added maisons

--- Databases/db.py
+++ Databases/db.py
@@ -67,6 +67,7 @@ def terrain():
             nom VARCHAR(50),
             prix INTEGER,
             loyer INTEGER,
+            maisons INTEGER,
             appartenance INTEGER,
             FOREIGN KEY(appartenance) REFERENCES joueurs(id_joueur));
         """)
@@ -81,24 +82,24 @@ def terrain():
         cursor = terrains.cursor()
         
         # {"Nom": (Prix, Loyer)}
-        liste_terrains = [{"Rue de Courcelles": (100, 550)},
-                        {"Avenue de la République": (120, 600)},
-                        {"Avenue de Noeuil": (140, 750)},
-                        {"Rue de Paradis": (160, 900)},
-                        {"Boulevard Saint-Michel": (180, 950)},
-                        {"Place Pigalle": (200, 1050)},
-                        {"Rue La Fayette": (250, 1200)},
-                        {"Boulevard des Cramptés": (300, 1500)},
-                        {"Avenue Fauché": (350, 1800)},
-                        {"Avenue Champs-NSI": (400, 2000)}]
+        liste_terrains = [{"Rue de Courcelles": (65, 550)},
+                        {"Avenue de la République": (70, 600)},
+                        {"Avenue de Noeuil": (105, 750)},
+                        {"Rue de Paradis": (110, 900)},
+                        {"Boulevard Saint-Michel": (160, 950)},
+                        {"Place Pigalle": (165, 1050)},
+                        {"Rue La Fayette": (215, 1200)},
+                        {"Boulevard des Cramptés": (230, 1500)},
+                        {"Avenue Fauché": (320, 1800)},
+                        {"Avenue Champs-NSI": (330, 2000)}]
         
         for i in liste_terrains:
             nom_rue = list(i.keys())[0]
             valeurs_rue = list(i.values())[0]
             cursor.execute("""
-            INSERT INTO Terrains(nom, prix, loyer)
-            VALUES(?, ?, ?);
-            """, (nom_rue, valeurs_rue[0], valeurs_rue[1]))
+            INSERT INTO Terrains(nom, prix, loyer, maisons)
+            VALUES(?, ?, ?, ?);
+            """, (nom_rue, valeurs_rue[0], valeurs_rue[1], 0))
             
         terrains.commit()
         
@@ -108,47 +109,5 @@ def terrain():
     creer_terrains()
     remplir_terrains()
     
-# def cartes_commu():    
-#     def creer_carte_commu():
-#         cartes_commu = sql.connect("Databases/cartes_commu.db")
-#         cursor = cartes_commu.cursor()
-        
-#         cursor.execute("""
-#         CREATE TABLE IF NOT EXISTS cartes_communautes(
-#            id_cartes INTEGER PRIMARY KEY AUTOINCREMENT,
-#            nom VARCHAR(50),
-#            effet VARCHAR(255));
-#         """)
-#         cartes_commu.commit()
-
-#         cursor.close()
-#         cartes_commu.close()
-
-#     def remplir_cartes_commu():
-#         cartes_commu = sql.connect("Databases/cartes_commu.db")
-#         cursor = cartes_commu.cursor()
-        
-#         #{"Nom": (effet)}
-#         liste_cartes_commu = [
-#             {}
-#         ]
-        
-#         for i in liste_cartes_commu:
-#             nom_cartes = list(i.keys())[0]
-#             effet_commu = list(i.values())[0]
-#             cursor.execute("""
-#             INSERT INTO cartes_commu(nom,effet)
-#             VALUES(?,?);
-#                            """,(nom_cartes,effet_commu))
-            
-#         cartes_commu.commit()
-
-#         cursor.close()
-#         cartes_commu.close()
-       
-#     creer_carte_commu() 
-#     remplir_cartes_commu()
-    
 joueur()
 terrain()
-# cartes_commu()
