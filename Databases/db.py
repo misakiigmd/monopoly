8fe2733f9689a676412ab810a4598dc4748365ba
misaki. <misakiigmd@gmail.com>
2024-03-25 15:20:50 +0100

    tkt

--- Databases/db.py
+++ Databases/db.py
@@ -1,98 +1,85 @@
 import os
+import random
 import sqlite3 as sql
+db_path = 'Databases\main.db'
 
 # Suppression de l'ancien fichier (si il existe) pour éviter l'insertion de la même donnée plusieurs fois
 def delete_old_file():
-    if os.path.exists('Databases/main.db'):
-        os.remove('Databases/main.db')
+    if os.path.exists(db_path):
+        os.remove(db_path)
         
-# Création et insertion des données dans la table CartesChance
-def carte_chance():
-        db = sql.connect('Databases/main.db')
-        cursor = db.cursor()
-
-        cursor.execute("""
-        CREATE TABLE IF NOT EXISTS CartesChance(
-            id_carte INTEGER PRIMARY KEY AUTOINCREMENT,
-            nom VARCHAR(50),
-            effet VARCHAR(255));
-        """)
-        
-        # {"nom": "effet"}
-        liste_cartes_chance = [{"CCH1": ""},
-                        {"CCH2": ""},
-                        {"CCH3": ""},
-                        {"CCH4": ""},
-                        {"CCH5": ""},
-                        {"CCH6": ""},
-                        {"CCH7": ""},
-                        {"CCH8": ""},
-                        {"CCH9": ""},
-                        {"CCH10": ""},
-                        {"CCH11": ""},
-                        {"CCH12": ""},
-                        {"CCH13": ""},
-                        {"CCH14": ""},
-                        {"CCH15": ""}]
-        
-        for i in liste_cartes_chance:
-            nom_carte_chance = list(i.keys())[0]
-            effet_carte_chance = list(i.values())[0]
-            cursor.execute("""
-            INSERT INTO CartesChance(nom, effet)
-            VALUES(?, ?);
-            """, (nom_carte_chance, effet_carte_chance))
-            
-        db.commit()
-        
-        cursor.close()
-        db.close()
-        
-# Création et insertion des données dans la table CartesCommunauté
-def carte_communauté():
-        db = sql.connect('Databases/main.db')
-        cursor = db.cursor()
-
-        cursor.execute("""
-        CREATE TABLE IF NOT EXISTS CartesCommunauté(
-            id_carte INTEGER PRIMARY KEY AUTOINCREMENT,
-            nom VARCHAR(50),
-            effet VARCHAR(255));
-        """)
-        
-        # {"nom": "effet"}
-        liste_cartes_communauté = [{"CCO1": "Images/cco1.png"},
-                        {"CCO2": "Images/cco2.png"},
-                        {"CCO3": "Images/cco3.png"},
-                        {"CCO4": "Images/cco4.png"},
-                        {"CCO5": "Images/cco5.png"},
-                        {"CCO6": "Images/cco6.png"},
-                        {"CCO7": "Images/cco7.png"},
-                        {"CCO8": "Images/cco8.png"},
-                        {"CCO9": "Images/cco9.png"},
-                        {"CCO10": "Images/cco10.png"},
-                        {"CCO11": "Images/cco11.png"},
-                        {"CCO12": "Images/cco12.png"},
-                        {"CCO13": "Images/cco13.png"}]
-        
-        for i in liste_cartes_communauté:
-            nom_carte_communauté = list(i.keys())[0]
-            effet_carte_communauté = list(i.values())[0]
-            cursor.execute("""
-            INSERT INTO CartesCommunauté(nom, effet)
-            VALUES(?, ?);
-            """, (nom_carte_communauté, effet_carte_communauté))
-            
-        db.commit()
-        
-        cursor.close()
-        db.close()
-    
-# Création et insertion des données dans la table Joueurs
-def joueur():
-    db = sql.connect('Databases/main.db')
+# Création et insertion des données dans les différentes tables
+def main():
+    # Connection au fichier de base de données
+    db = sql.connect(db_path)
     cursor = db.cursor()
+
+    # Création de la table CartesChance
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS CartesChance(
+        id_carte INTEGER PRIMARY KEY AUTOINCREMENT,
+        nom VARCHAR(50),
+        chemin VARCHAR(50));
+    """)
     
+    # {"nom": "chemin"}
+    liste_cartes_chance = [{"CCH1": "Images/cch1.png"},
+                            {"CCH2": "Images/cch2.png"},
+                            {"CCH3": "Images/cch3.png"},
+                            {"CCH4": "Images/cch4.png"},
+                            {"CCH5": "Images/cch5.PNG"},
+                            {"CCH6": "Images/cch6.PNG"},
+                            {"CCH7": "Images/cch7.PNG"},
+                            {"CCH8": "Images/cch8.PNG"},
+                            {"CCH9": "Images/cch9.PNG"},
+                            {"CCH10": "Images/cch10.PNG"},
+                            {"CCH11": "Images/cch11.PNG"},
+                            {"CCH12": "Images/cch12.PNG"},
+                            {"CCH13": "Images/cch13.PNG"}]
+    
+    # Insertion des données définies au dessus
+    for i in liste_cartes_chance:
+        nom_carte_chance = list(i.keys())[0]
+        chemin_carte_chance = list(i.values())[0]
+        cursor.execute("""
+        INSERT INTO CartesChance(nom, chemin)
+        VALUES(?, ?);
+        """, (nom_carte_chance, chemin_carte_chance))
+        
+    # Création de la table CartesCommunauté
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS CartesCommunauté(
+        id_carte INTEGER PRIMARY KEY AUTOINCREMENT,
+        nom VARCHAR(50),
+        chemin VARCHAR(50),
+        effet VARCHAR(255));
+    """)
+    
+    # {"nom": ("chemin", "effet")}
+    liste_cartes_communauté = [{"CCO1": ("Images/cco1.png", "Vous êtes à l'hôpital, malheureusement on est aux USA donc pas de carte vitale. Payer 300D.")},
+        {"CCO2": ("Images/cco2.png", "Vous êtes allé au médecin, malheureusement on est aux USA donc pas de carte vitale. Payer 50D.")},
+        {"CCO3": ("Images/cco3.png", "C'est l'heure de payer le loyer. Si tu ne veux pas que les huissiers arrivent, payer 200D.")},
+        {"CCO4": ("Images/cco4.png", "Erreur de la banque par un hasard de circonstance, la banque vous offre de l'argent. Vous avez de la chance. Recevez 25D.")},
+        {"CCO5": ("Images/cco5.png", "Erreur de la banque par un hasard de circonstance, la banque vous offre de l'argent. Vous avez de la chance. Recevez 75D.")},
+        {"CCO6": ("Images/cco6.png", "Erreur de la banque par un hasard de circonstance, la banque vous offre de l'argent. Vous avez de la chance. Recevez 100D.")},
+        {"CCO7": ("Images/cco7.png", "Vous êtes arrivé premier au grand prix de beauté. Vous êtes magnifique. Recevez 150D.")},
+        {"CCO8": ("Images/cco8.png", "Mamie est morte, c'est triste oui, mais rappelez-vous, l'héritage de Mamie est conséquent. Allez, fêtons votre nouvelle fortune. Recevez 200D.")},
+        {"CCO9": ("Images/cco9.png", "Vous êtes arrivé premier au grand prix de mots croisés. Bravo. Recevez 50D.")},
+        {"CCO10": ("Images/cco10.png", "Vous êtes un hors-la-loi. Allez en prison sans passer par la case départ.")},
+        {"CCO11": ("Images/cco11.png", "Bonjour, nous sommes les taxes. Merci de bien vouloir payer 25D pour chaque maison et 50M pour chaque hôtel. Passez une bonne journée.")},
+        {"CCO12": ("Images/cco12.png", "Bonjour, nous sommes les taxes. Merci de bien vouloir payer 50D pour chaque maison et 100M pour chaque hôtel. Passez une bonne journée.")},
+        {"CCO13": ("Images/cco13.png", "Erreur de la banque par un hasard de circonstance, la banque vous offre de l'argent. Vous avez de la chance. Recevez 50D.")}]
+    
+    # Insertion des données définies au dessus
+    for i in liste_cartes_communauté:
+        nom_carte_communauté = list(i.keys())[0]
+        carte_communauté = list(i.values())[0]
+        cursor.execute("""
+        INSERT INTO CartesCommunauté(nom, chemin, effet)
+        VALUES(?, ?, ?);
+        """, (nom_carte_communauté, carte_communauté[0], carte_communauté[1]))
+            
+    # Création de la table Joueurs
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS Joueurs(
         id_joueur INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -108,25 +95,17 @@ def joueur():
     
     nombre_joueurs = 2
     
+    # Insertion du nombre de billets et de carte prisons de base autant de fois qu'il y a de joueurs
     for i in range(nombre_joueurs):
         cursor.execute("""
         INSERT INTO Joueurs(billets_500, billets_100, billets_50, billets_20, billets_10, billets_5, billets_1, cartes_prisons)
         VALUES(?, ?, ?, ?, ?, ?, ?, ?);
         """, (1, 1, 1, 2, 3, 5, 5, 0))
-        
-    db.commit()
-    
-    cursor.close()
-    db.close()
-        
-# Création et insertion des données dans la table Terrains
-def terrain():
-    db = sql.connect('Databases/main.db')
-    cursor = db.cursor()
 
+    # Création de la table Terrains
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS Terrains(
-        id_terrain INTEGER PRIMARY KEY AUTOINCREMENT,
+        id_terrain INTEGER PRIMARY KEY,
         nom VARCHAR(50),
         prix INTEGER,
         loyer INTEGER,
@@ -137,33 +116,79 @@ def terrain():
     
     # {"nom": (prix, loyer)}
     # Le loyer correspond au loyer de base (il dépend du nombre de maisons)
-    liste_terrains = [{"Rue de Courcelles": (65, 30)},
-                    {"Avenue de la République": (70, 40)},
-                    {"Avenue de Noeuil": (105, 50)},
-                    {"Rue de Paradis": (110, 60)},
-                    {"Boulevard Saint-Michel": (160, 70)},
-                    {"Place Pigalle": (165, 80)},
-                    {"Rue La Fayette": (215, 120)},
-                    {"Boulevard des Cramptés": (230, 130)},
-                    {"Avenue Fauché": (320, 170)},
-                    {"Avenue Champs-NSI": (330, 180)}]
+    liste_terrains = [{"Rue de Courcelles": (1, 65, 30)},
+                    {"Avenue de la République": (2, 70, 40)},
+                    {"Avenue de Noeuil": (4, 105, 50)},
+                    {"Rue de Paradis": (6, 110, 60)},
+                    {"Boulevard Saint-Michel": (8, 160, 70)},
+                    {"Place Pigalle": (9, 165, 80)},
+                    {"Rue La Fayette": (12, 15, 120)},
+                    {"Boulevard des Cramptés": (13, 230, 130)},
+                    {"Avenue Fauché": (17, 320, 170)},
+                    {"Avenue Champs-NSI": (18, 330, 180)}]
     
+    # Insertion des données définies au dessus, ainsi que le nombre de maisons de base (0)
     for i in liste_terrains:
         nom_rue = list(i.keys())[0]
         valeurs_rue = list(i.values())[0]
         cursor.execute("""
-        INSERT INTO Terrains(nom, prix, loyer, maisons)
-        VALUES(?, ?, ?, ?);
-        """, (nom_rue, valeurs_rue[0], valeurs_rue[1], 0))
+        INSERT INTO Terrains(id_terrain, nom, prix, loyer, maisons)
+        VALUES(?, ?, ?, ?, ?);
+        """, (valeurs_rue[0], nom_rue, valeurs_rue[1], valeurs_rue[2], 0))
         
     db.commit()
     
     cursor.close()
     db.close()
+    
+def billets(id):
+    db = sql.connect(db_path)
+    cursor = db.cursor()
+    
+    cursor.execute("""
+    SELECT billets_500, billets_100, billets_50, billets_20, billets_10, billets_5, billets_1 FROM Joueurs
+    WHERE id_joueur = ?""", (str(id)))
+    
+    result = cursor.fetchone() 
+    
+    # Transformation du tuple en liste
+    result_list = list(result)
+    
+    cursor.close()
+    db.close()
+    
+    print(result_list)
         
+def carteco_random():
+    db = sql.connect(db_path)
+    cursor = db.cursor()
+    
+    cursor.execute("""
+    SELECT id_carte, chemin FROM CartesCommunauté""")
+    
+    results = cursor.fetchall()
+    carte = random.randint(0, len(results) - 1)
+        
+    print(results[carte])
+    
+    cursor.close()
+    db.close()
+    
+def cartech_random():
+    db = sql.connect(db_path)
+    cursor = db.cursor()
+    
+    cursor.execute("""
+    SELECT id_carte, chemin FROM CartesChance""")
+    
+    results = cursor.fetchall()
+    carte = random.randint(0, len(results) - 1)
+        
+    print(results[carte])
+    
+    cursor.close()
+    db.close()
+    
 if __name__ == "__main__":
     delete_old_file()
-    carte_communauté()
-    carte_chance()
-    joueur()
-    terrain()
+    main()
