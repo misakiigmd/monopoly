12c0431a3305a20b133a03f1723c38a241d92416
misaki. <misakiigmd@gmail.com>
2024-03-25 13:50:32 +0100

    ID C BON

--- Databases/db.py
+++ Databases/db.py
@@ -1,17 +1,18 @@
 import os
 import sqlite3 as sql
+db_name = 'Databases/main.db'
 
 # Suppression de l'ancien fichier (si il existe) pour éviter l'insertion de la même donnée plusieurs fois
 def delete_old_file():
-    if os.path.exists('Databases/main.db'):
-        os.remove('Databases/main.db')
+    if os.path.exists(db_name):
+        os.remove(db_name)
         
 # Création et insertion des données dans les différentes tables
 def main():
     delete_old_file()
     
     # Connection au fichier de base de données
-    db = sql.connect('Databases/main.db')
+    db = sql.connect(db_name)
     cursor = db.cursor()
 
     # Création de la table CartesChance
@@ -108,7 +109,7 @@ def main():
     # Création de la table Terrains
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS Terrains(
-        id_terrain INTEGER PRIMARY KEY AUTOINCREMENT,
+        id_terrain INTEGER PRIMARY KEY,
         nom VARCHAR(50),
         prix INTEGER,
         loyer INTEGER,
@@ -119,25 +120,25 @@ def main():
     
     # {"nom": (prix, loyer)}
     # Le loyer correspond au loyer de base (il dépend du nombre de maisons)
-    liste_terrains = [{"Rue de Courcelles": (65, 30)},
-                    {"Avenue de la République": (70, 40)},
-                    {"Avenue de Noeuil": (105, 50)},
-                    {"Rue de Paradis": (110, 60)},
-                    {"Boulevard Saint-Michel": (160, 70)},
-                    {"Place Pigalle": (165, 80)},
-                    {"Rue La Fayette": (215, 120)},
-                    {"Boulevard des Cramptés": (230, 130)},
-                    {"Avenue Fauché": (320, 170)},
-                    {"Avenue Champs-NSI": (330, 180)}]
+    liste_terrains = [{"Rue de Courcelles": (1, 65, 30)},
+                    {"Avenue de la République": (2, 70, 40)},
+                    {"Avenue de Noeuil": (4, 105, 50)},
+                    {"Rue de Paradis": (6, 110, 60)},
+                    {"Boulevard Saint-Michel": (8, 160, 70)},
+                    {"Place Pigalle": (9, 165, 80)},
+                    {"Rue La Fayette": (12, 15, 120)},
+                    {"Boulevard des Cramptés": (13, 230, 130)},
+                    {"Avenue Fauché": (17, 320, 170)},
+                    {"Avenue Champs-NSI": (18, 330, 180)}]
     
     # Insertion des données définies au dessus, ainsi que le nombre de maisons de base (0)
     for i in liste_terrains:
         nom_rue = list(i.keys())[0]
         valeurs_rue = list(i.values())[0]
         cursor.execute("""
-        INSERT INTO Terrains(nom, prix, loyer, maisons)
-        VALUES(?, ?, ?, ?);
-        """, (nom_rue, valeurs_rue[0], valeurs_rue[1], 0))
+        INSERT INTO Terrains(id_terrain, nom, prix, loyer, maisons)
+        VALUES(?, ?, ?, ?, ?);
+        """, (valeurs_rue[0], nom_rue, valeurs_rue[1], valeurs_rue[2], 0))
         
     db.commit()
     
