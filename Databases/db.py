874e8cd67f30eb4fa2328e8d21caf92a878ec9b8
misaki. <misakiigmd@gmail.com>
2024-03-25 11:15:19 +0100

    Moved every table creation to one function

--- Databases/db.py
+++ Databases/db.py
@@ -6,95 +6,76 @@ def delete_old_file():
     if os.path.exists('Databases/main.db'):
         os.remove('Databases/main.db')
         
-# Création et insertion des données dans la table CartesChance
-def carte_chance():
-        db = sql.connect('Databases/main.db')
-        cursor = db.cursor()
-
-        cursor.execute("""
-        CREATE TABLE IF NOT EXISTS CartesChance(
-            id_carte INTEGER PRIMARY KEY AUTOINCREMENT,
-            nom VARCHAR(50),
-            effet VARCHAR(255));
-        """)
-        
-        # {"nom": "effet"}
-        liste_cartes_chance = [{"CCH1": ""},
-                        {"CCH2": ""},
-                        {"CCH3": ""},
-                        {"CCH4": ""},
-                        {"CCH5": ""},
-                        {"CCH6": ""},
-                        {"CCH7": ""},
-                        {"CCH8": ""},
-                        {"CCH9": ""},
-                        {"CCH10": ""},
-                        {"CCH11": ""},
-                        {"CCH12": ""},
-                        {"CCH13": ""},
-                        {"CCH14": ""},
-                        {"CCH15": ""}]
-        
-        for i in liste_cartes_chance:
-            nom_carte_chance = list(i.keys())[0]
-            effet_carte_chance = list(i.values())[0]
-            cursor.execute("""
-            INSERT INTO CartesChance(nom, effet)
-            VALUES(?, ?);
-            """, (nom_carte_chance, effet_carte_chance))
-            
-        db.commit()
-        
-        cursor.close()
-        db.close()
-        
-# Création et insertion des données dans la table CartesCommunauté
-def carte_communauté():
-        db = sql.connect('Databases/main.db')
-        cursor = db.cursor()
-
-        cursor.execute("""
-        CREATE TABLE IF NOT EXISTS CartesCommunauté(
-            id_carte INTEGER PRIMARY KEY AUTOINCREMENT,
-            nom VARCHAR(50),
-            effet VARCHAR(255));
-        """)
-        
-        # {"nom": "effet"}
-        liste_cartes_communauté = [{"CCO1": ""},
-                        {"CCO2": ""},
-                        {"CCO3": ""},
-                        {"CCO4": ""},
-                        {"CCO5": ""},
-                        {"CCO6": ""},
-                        {"CCO7": ""},
-                        {"CCO8": ""},
-                        {"CCO9": ""},
-                        {"CCO10": ""},
-                        {"CCO11": ""},
-                        {"CCO12": ""},
-                        {"CCO13": ""},
-                        {"CCO14": ""},
-                        {"CCO15": ""}]
-        
-        for i in liste_cartes_communauté:
-            nom_carte_communauté = list(i.keys())[0]
-            effet_carte_communauté = list(i.values())[0]
-            cursor.execute("""
-            INSERT INTO CartesCommunauté(nom, effet)
-            VALUES(?, ?);
-            """, (nom_carte_communauté, effet_carte_communauté))
-            
-        db.commit()
-        
-        cursor.close()
-        db.close()
+def main():
+    delete_old_file()
     
-# Création et insertion des données dans la table Joueurs
-def joueur():
     db = sql.connect('Databases/main.db')
     cursor = db.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS CartesChance(
+        id_carte INTEGER PRIMARY KEY AUTOINCREMENT,
+        nom VARCHAR(50),
+        effet VARCHAR(255));
+    """)
     
+    # {"nom": "effet"}
+    liste_cartes_chance = [{"CCH1": ""},
+                    {"CCH2": ""},
+                    {"CCH3": ""},
+                    {"CCH4": ""},
+                    {"CCH5": ""},
+                    {"CCH6": ""},
+                    {"CCH7": ""},
+                    {"CCH8": ""},
+                    {"CCH9": ""},
+                    {"CCH10": ""},
+                    {"CCH11": ""},
+                    {"CCH12": ""},
+                    {"CCH13": ""},
+                    {"CCH14": ""},
+                    {"CCH15": ""}]
+    
+    for i in liste_cartes_chance:
+        nom_carte_chance = list(i.keys())[0]
+        effet_carte_chance = list(i.values())[0]
+        cursor.execute("""
+        INSERT INTO CartesChance(nom, effet)
+        VALUES(?, ?);
+        """, (nom_carte_chance, effet_carte_chance))
+        
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS CartesCommunauté(
+        id_carte INTEGER PRIMARY KEY AUTOINCREMENT,
+        nom VARCHAR(50),
+        effet VARCHAR(255));
+    """)
+    
+    # {"nom": "effet"}
+    liste_cartes_communauté = [{"CCO1": ""},
+                    {"CCO2": ""},
+                    {"CCO3": ""},
+                    {"CCO4": ""},
+                    {"CCO5": ""},
+                    {"CCO6": ""},
+                    {"CCO7": ""},
+                    {"CCO8": ""},
+                    {"CCO9": ""},
+                    {"CCO10": ""},
+                    {"CCO11": ""},
+                    {"CCO12": ""},
+                    {"CCO13": ""},
+                    {"CCO14": ""},
+                    {"CCO15": ""}]
+    
+    for i in liste_cartes_communauté:
+        nom_carte_communauté = list(i.keys())[0]
+        effet_carte_communauté = list(i.values())[0]
+        cursor.execute("""
+        INSERT INTO CartesCommunauté(nom, effet)
+        VALUES(?, ?);
+        """, (nom_carte_communauté, effet_carte_communauté))
+            
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS Joueurs(
         id_joueur INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -115,16 +96,6 @@ def joueur():
         INSERT INTO Joueurs(billets_500, billets_100, billets_50, billets_20, billets_10, billets_5, billets_1, cartes_prisons)
         VALUES(?, ?, ?, ?, ?, ?, ?, ?);
         """, (1, 1, 1, 2, 3, 5, 5, 0))
-        
-    db.commit()
-    
-    cursor.close()
-    db.close()
-        
-# Création et insertion des données dans la table Terrains
-def terrain():
-    db = sql.connect('Databases/main.db')
-    cursor = db.cursor()
 
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS Terrains(
@@ -164,8 +135,4 @@ def terrain():
     db.close()
         
 if __name__ == "__main__":
-    delete_old_file()
-    carte_communauté()
-    carte_chance()
-    joueur()
-    terrain()
+    main()
