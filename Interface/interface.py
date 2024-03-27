61e23bc282653d003cbf23ac7a8f6b08e4a963ea
Nagro0m <warlockworldtm@gmail.com>
2024-03-27 08:46:32 +0100

    Update interface.py

--- Interface/interface.py
+++ Interface/interface.py
@@ -2,8 +2,10 @@ import tkinter as tk
 from tkinter import messagebox
 from time import sleep
 import sqlite3 as sql
-db_name = 'Databases/main.db'
 from coordonnées import coord
+
+db_name = 'Databases/main.db'
+
 
 class MonopolyPlateau(tk.Tk):
     def __init__(self, size=6):
@@ -11,7 +13,8 @@ class MonopolyPlateau(tk.Tk):
 
         self.size = size
         self.pieces = []  # Liste pour stocker les références aux pièces créées
-
+        self.piece1_case = {0: (4.5, 7), 1: (4.5, 6), 2: (4.5, 5), 3: (4.5, 4), 4: (4.5, 3), 5: (4.5, 2), 6: (3.5, 2), 7: (2.5, 2), 8: (1.5, 2), 9: (0.5, 2), 10: (-0.5, 2), 11: (-0.5, 3), 12: (-0.5, 4), 13: (-0.5, 5), 14: (-0.5, 6), 15: (-0.5, 7), 16: (0.5, 7), 17: (1.5, 7), 18: (2.5, 7), 19: (3.5, 7)}
+        self.piece1_number = 0
         self.attributes('-fullscreen', True)
         self.bind('<Escape>', lambda e: self.destroy())
 
@@ -47,12 +50,12 @@ class MonopolyPlateau(tk.Tk):
             for j in range(size):
                 if i == 0 or i == size - 1 or j == 0 or j == size - 1:
                     square = tk.Canvas(self, width=square_size, height=square_size, highlightthickness=0, bg="lightgray")
-                    square.grid(row=i, column=j+1, padx=5, pady=5)
+                    square.grid(row=i, column=j+1, padx=2, pady=1)
                     label = tk.Label(square, text=f"{coord[i*size + j + 1]}")
                     label.place(relx=0.5, rely=0.5, anchor="c")
 
-        self.place_piece(size - 1, size + 1, "lightgreen")
-        self.place_piece(size - 0.5, size + 1, "lightblue")
+        self.place_piece(size - 1.5, size + 1, "lightgreen")
+        self.place_piece(size - 1, size + 1, "lightblue")
 
         player1_column = tk.Canvas(self, width=100, height=window_height, bg="lightblue")
         player1_column.grid(row=0, column=0, rowspan=size, padx=50, pady=50)
@@ -73,15 +76,22 @@ class MonopolyPlateau(tk.Tk):
             if text == "Acheter":
                 button.config(command=lambda: remove_button(button))
             if text == "Passer":
-                button.config(command=lambda: move_piece(self.pieces[0][0], self.pieces[0][1], self.pieces[0][2]-1, 0))
-                
-        @staticmethod        
-        def move_piece(piece, new_row, new_col, piece_num):
+                button.config(command=lambda: move_piece(0))
+
+        @staticmethod 
+        def move_piece(piece_num): 
+            print(self.piece1_number) 
+            if (self.piece1_number == 18):
+                self.piece1_number = 0
+            else:
+                self.piece1_number += 1
             square_size = 100
-            piece_size = 30
-            piece.place(x=new_col * square_size + square_size / 2 + 5 * (new_col + 1) - piece_size / 2, y=new_row * square_size + square_size / 2 + 5 * (new_row + 1) - piece_size / 2, anchor="c")
+            piece, old_row, old_col = self.pieces[piece_num]
+            new_row, new_col = self.piece1_case[self.piece1_number]
+            piece.place(x=new_col * square_size + square_size / 2 + 5 * (new_col + 1), y=new_row * square_size + square_size / 2 + 5 * (new_row + 1), anchor="c")
             self.pieces[piece_num] = [piece, new_row, new_col]
-            
+            print(new_row, new_col)
+
         def remove_button(button):
             button.pack_forget()
 
